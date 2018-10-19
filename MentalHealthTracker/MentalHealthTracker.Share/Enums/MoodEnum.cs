129f879bb03c609eabce34e5c1f84a1fda6e4ef1
profrobbins <profrobbins@users.noreply.github.com>
2018-10-18 19:50:32 -0500

    created enum and form controller

---
+++ MentalHealthTracker/MentalHealthTracker.Share/Enums/MoodEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentalHealthTracker.Share.Entities
+{
+   public enum MoodEnum
+    {
+        Bad,
+        Sad,
+        Glad,
+        Enthusiastic,
+        happy
+
+
+    }
+}
