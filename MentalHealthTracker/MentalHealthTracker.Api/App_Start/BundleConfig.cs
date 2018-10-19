4fbfeee0b560df91ca7343d376e282ac88f35df9
Alek Brown <36023175+A-J-Brown@users.noreply.github.com>
2018-10-18 21:04:01 -0500

    Fixed Missing References
    AspNet references missing. repaired packages. some remain but are unused in project.

--- MentalHealthTracker/MentalHealthTracker.Api/App_Start/BundleConfig.cs
+++ MentalHealthTracker/MentalHealthTracker.Api/App_Start/BundleConfig.cs
@@ -1,12 +1,16 @@
 ﻿using System.Web;
+#pragma warning disable CS0234 // The type or namespace name 'Optimization' does not exist in the namespace 'System.Web' (are you missing an assembly reference?)
 using System.Web.Optimization;
+#pragma warning restore CS0234 // The type or namespace name 'Optimization' does not exist in the namespace 'System.Web' (are you missing an assembly reference?)
 
 namespace MentalHealthTracker.Api
 {
     public class BundleConfig
     {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+#pragma warning disable CS0246 // The type or namespace name 'BundleCollection' could not be found (are you missing a using directive or an assembly reference?)
         public static void RegisterBundles(BundleCollection bundles)
+#pragma warning restore CS0246 // The type or namespace name 'BundleCollection' could not be found (are you missing a using directive or an assembly reference?)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
--- MentalHealthTracker/MentalHealthTracker.Api/Controllers/HomeController.cs
+++ MentalHealthTracker/MentalHealthTracker.Api/Controllers/HomeController.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+#pragma warning disable CS0234 // The type or namespace name 'Mvc' does not exist in the namespace 'System.Web' (are you missing an assembly reference?)
 using System.Web.Mvc;
+#pragma warning restore CS0234 // The type or namespace name 'Mvc' does not exist in the namespace 'System.Web' (are you missing an assembly reference?)
 
 namespace MentalHealthTracker.Api.Controllers
 {
+#pragma warning disable CS0246 // The type or namespace name 'Controller' could not be found (are you missing a using directive or an assembly reference?)
     public class HomeController : Controller
+#pragma warning restore CS0246 // The type or namespace name 'Controller' could not be found (are you missing a using directive or an assembly reference?)
     {
+#pragma warning disable CS0246 // The type or namespace name 'ActionResult' could not be found (are you missing a using directive or an assembly reference?)
         public ActionResult Index()
+#pragma warning restore CS0246 // The type or namespace name 'ActionResult' could not be found (are you missing a using directive or an assembly reference?)
         {
             ViewBag.Title = "Home Page";
 
