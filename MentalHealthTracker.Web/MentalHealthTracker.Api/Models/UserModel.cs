32789e4a4b0ca36369cbbc462429bd795185e5d5
cbuchwald229 <31973954+cbuchwald229@users.noreply.github.com>
2018-10-30 18:33:39 -0500

    Controllers added in Api

---
+++ MentalHealthTracker.Web/MentalHealthTracker.Api/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MentalHealthTracker.Api.Models
+{
+    public class UserModel
+    {
+        public Guid UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
