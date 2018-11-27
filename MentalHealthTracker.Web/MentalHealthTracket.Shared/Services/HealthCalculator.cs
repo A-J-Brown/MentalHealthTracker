f8896003924032b3b14e65cea26a69b3993d71af
profrobbins <profrobbins@users.noreply.github.com>
2018-11-26 22:30:58 -0600

    added service

---
+++ MentalHealthTracker.Web/MentalHealthTracket.Shared/Services/HealthCalculator.cs
@@ -0,0 +1,19 @@
+﻿using MentalHealthTracket.Shared.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentalHealthTracket.Shared.Services
+{
+   public class HealthCalculator : IHealthCalculator
+
+    {
+        public int MoodAverage(int[] moods)
+        {
+            var average = moods.Average();
+            return (int)Math.Round(average, 0);
+        }
+    }
+}
